eb426d430df11336a2f109a0de4793b60a5d2c1e
amr.metwally <magdyamr542@gmail.com>
2021-07-28 00:29:22 +0200

    added events that are fired when user types or the cursor moves

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const getCurrentLine = () => {
+  const document = vscode.window.activeTextEditor?.document;
+  if (document) {
+    return document.getText();
+  }
+};
+
+const getCursorPos = () => {
+  return vscode.window.activeTextEditor?.selection.active.character;
+};
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vim-find-highlight" is now active!');
+  // register event when the user types something
+  vscode.workspace.onDidChangeTextDocument((e) => {
+    console.log("User changed text of document ");
+    const text = getCurrentLine();
+    const cursorPos = getCursorPos();
+    let currentChar = "";
+    if (text && cursorPos) {
+      currentChar = text[cursorPos];
+    }
+    console.log({ text, cursorPos, currentChar });
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vim-find-highlight.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vim-find-highlight!');
-	});
-
-	context.subscriptions.push(disposable);
+  vscode.window.onDidChangeTextEditorSelection((e) => {
+    console.log("Changing selection");
+    const text = getCurrentLine();
+    const cursorPos = getCursorPos();
+    let currentChar = "";
+    if (text && cursorPos) {
+      currentChar = text[cursorPos];
+    }
+    console.log({ text, cursorPos, currentChar });
+    console.log("Event", e);
+  });
 }
 
 // this method is called when your extension is deactivated
