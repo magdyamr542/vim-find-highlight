ed52addce31f2e440e1ec21116c7500a6b489e25
amr.metwally <magdyamr542@gmail.com>
2021-07-29 23:42:26 +0200

    refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,9 @@
 import * as vscode from "vscode";
-import { disposeCharDecoration, getCharDecoration } from "./decoration";
-import { isAlphabetic } from "./utils";
+import { charHighlighter } from "./CharHighlighter";
+import { disposeCharDecoration } from "./decoration";
+import { colorChars, getCurrentLine, getCursorPos } from "./utils";
 
-interface CharPosition {
-	positions: number[];
-}
-interface CharColoring {
-	color: string;
-	position: number;
-}
-
-interface WordWithIndex {
-	word: string;
-	startIndex: number;
-}
-
-interface WordWithIndexWithCompareFunc extends WordWithIndex {
-	compare: (charPos: number, cursorPos: number, actualPos: number) => boolean;
-}
-
-const getCurrentLine = () => {
-	const activeEditor = getActiveEditor();
-	const res = activeEditor?.document.lineAt(activeEditor?.selection.active.line);
-	return res;
-};
-
-const getActiveEditor = () => {
-	return vscode.window.activeTextEditor;
-}
-
-const getCursorPos = () => {
-	return getActiveEditor()?.selection.active.character;
-
-};
-
-export function activate(context: vscode.ExtensionContext) {
+export function activate(_: vscode.ExtensionContext) {
 	let timeout: NodeJS.Timer | undefined = undefined;
 
 	const debouncedMain = (cursorPos: number, text: string) => {
@@ -45,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
 		timeout = setTimeout(() => main(cursorPos, text), 500);
 	}
 
-	// register event when the user types something
+	// when the user types
 	vscode.workspace.onDidChangeTextDocument((e) => {
 		const line = getCurrentLine();
 		const cursorPos = getCursorPos();
@@ -56,6 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
+	// when the cursor moves 
 	vscode.window.onDidChangeTextEditorSelection((e) => {
 		const line = getCurrentLine();
 		const cursorPos = e.textEditor.selection.active.character;
@@ -68,127 +38,11 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 const main = (cursorPos: number, currentLine: string) => {
-	const frequencyMap = getCharFrequencyMap(currentLine);
-	const toColorAfter = getCharPosToColor(frequencyMap, currentLine, cursorPos);
-	for (const char of toColorAfter) {
-		console.log(`will color char ${currentLine[char.position]}`);
-	}
-	colorChars(toColorAfter)
-};
-
-
-function colorChars(toColor: CharColoring[]) {
-	const editor = getActiveEditor();
-	if (!editor) {
-		return;
-	}
-	const decorations: vscode.DecorationOptions[] = [];
-	const line = getCurrentLine()
-	for (const word of toColor) {
-		if (line) {
-			const startPos = new vscode.Position(line.lineNumber, word.position)
-			const endPos = new vscode.Position(line.lineNumber, word.position + 1)
-			decorations.push({ range: new vscode.Range(startPos, endPos) })
-		} else {
-			console.log("No line return");
-		}
-	}
-	editor.setDecorations(getCharDecoration(), decorations);
-}
-
-const getCharPosToColor = (
-	frequencyMap: Map<string, CharPosition>,
-	text: string,
-	cursorPos: number
-): CharColoring[] => {
-	// for each word select index of char which should be colored
-	const result: CharColoring[] = [];
-	const wordsWithIndexes = getWordsWithIndexes(text);
-	if (wordsWithIndexes.length == 0) {
-		return [];
-	}
-	const wordsAfterCursor = wordsWithIndexes.filter((word) => word.startIndex > cursorPos);
-	const wordsBeforeCursor = wordsWithIndexes.filter((word) => word.startIndex + word.word.length < cursorPos);
-
-	const before: WordWithIndexWithCompareFunc[] = wordsBeforeCursor.map(w => {
-		return { word: w.word, startIndex: w.startIndex, compare: (charPos, cursorPosition, actualPos) => charPos < cursorPosition && charPos >= actualPos }
-	})
-	const after: WordWithIndexWithCompareFunc[] = wordsAfterCursor.map(w => {
-		return { word: w.word, startIndex: w.startIndex, compare: (charPos, cursorPosition, actualPos) => charPos > cursorPosition && charPos <= actualPos }
-	})
-
-	for (const word of before.reverse().concat(after)) {
-		result.push(
-			getCharColoring(frequencyMap, word, cursorPos)
-		);
-	}
-	return result.filter(w => w.position !== -1);
-};
-
-const getCharColoring = (
-	frequencyMap: Map<string, CharPosition>,
-	word: WordWithIndexWithCompareFunc,
-	cursorPos: number
-): CharColoring => {
-
-	let minFreqForChar = Number.MAX_VALUE;
-	let indexOfCharWithMinFreq = -1;
-
-	for (const [index, char] of word.word.split("").entries()) {
-
-		const mapHasChar = frequencyMap.has(char);
-		const actualPos = word.startIndex + index;
-
-		if (!mapHasChar) {
-			return { color: "red", position: actualPos }; // this char is okay to use to reach the word
-		}
-
-		const positions = frequencyMap.get(char);
-		const freq = positions!.positions.filter(p => word.compare(p, cursorPos, actualPos)).length; // all occurrences of the char after the cursor
-
-		if (freq === 0) {
-			return { color: "red", position: actualPos }; // this char is okay to use to reach the word
-		}
-
-		// we can not reach the word using this char with one jump so maybe it works with next char.
-		if (freq < minFreqForChar) {
-			minFreqForChar = freq;
-			indexOfCharWithMinFreq = actualPos;
-		}
-	}
-
-	return { color: "red", position: indexOfCharWithMinFreq };
-
-}
-const getWordsWithIndexes = (text: string): WordWithIndex[] => {
-	const allWords = text.split(/(\W)/gi).reduce<{ word: string; startIndex: number }[]>(
-		(prev, currWord, index) => {
-			if (index === 0) {
-				prev.push({ word: currWord, startIndex: 0 });
-				return prev;
-			}
-			const startIndex =
-				prev[index - 1].startIndex + prev[index - 1].word.length;
-			prev.push({ word: currWord, startIndex });
-			return prev;
-		},
-		[]
-	);
-	return allWords.filter(word => isAlphabetic(word.word));
-};
-// returns for every char where it has been seen before
-const getCharFrequencyMap = (text: string) => {
-	const map: Map<string, CharPosition> = new Map();
-	text.split("").forEach((char, index) => {
-		if (map.has(char)) {
-			map.set(char, { positions: [...map.get(char)!.positions, index] });
-		} else {
-			map.set(char, { positions: [index] });
-		}
-	});
-	return map;
+	const toColor = charHighlighter.getCharHighlighting(currentLine, cursorPos);
+	colorChars(toColor);
 };
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
+
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,41 @@
+import * as vscode from "vscode";
+import { CharColoring } from "./CharHighlighter";
+import { getCharDecoration } from "./decoration";
 
 export const isAlphabetic = (str: string) => {
 	const regex = /\w/gi
 	return regex.test(str)
+}
+
+export const getCurrentLine = () => {
+	const activeEditor = getActiveEditor();
+	const res = activeEditor?.document.lineAt(activeEditor?.selection.active.line);
+	return res;
+};
+
+export const getActiveEditor = () => {
+	return vscode.window.activeTextEditor;
+}
+
+export const getCursorPos = () => {
+	return getActiveEditor()?.selection.active.character;
+};
+
+export const colorChars = (toColor: CharColoring[]) => {
+	const editor = getActiveEditor();
+	if (!editor) {
+		return;
+	}
+	const decorations: vscode.DecorationOptions[] = [];
+	const line = getCurrentLine()
+	for (const word of toColor) {
+		if (line) {
+			const startPos = new vscode.Position(line.lineNumber, word.position)
+			const endPos = new vscode.Position(line.lineNumber, word.position + 1)
+			decorations.push({ range: new vscode.Range(startPos, endPos) })
+		} else {
+			console.log("No line return");
+		}
+	}
+	editor.setDecorations(getCharDecoration(), decorations);
 }
